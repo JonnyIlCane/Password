bb1c29c4550e32e1c58e6bcf78dc01860c36409a
Pablitoz <59056235+JonnyIlCane@users.noreply.github.com>
2019-12-20 14:50:39 +0100

    Update passgen_english.py

--- passgen_english.py
+++ passgen_english.py
@@ -1,9 +1,8 @@
 import random
-letters=['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','i','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0']
+import string
+letters=list(string.punctuation)+list(string.ascii_letters)+list(string.ascii_letters)
 length=int(input('how long does the password have to be?'))
-a=0
 password=''
-while a<length:
+for x in range(0,length):
   password=password+(letters[random.randint(0,len(letters)-1)])
-  a=a+1
 print(password)
